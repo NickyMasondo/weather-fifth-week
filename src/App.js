6ee53dd18217551651b20c910b2b6fec40d8749d
NickyMasondo <nickymasondo@gmail.com>
2024-07-15 23:00:34 +0200

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="Durban" />
+        <footer>
+          <span> This project was coded by </span>
+          <a
+            href="https://https://github.com/NickyMasondo"
+            target="blank"
+            rel="noreferrer"
+          >
+            Sinikeziwe Masondo
+          </a>
+          <span> and is </span>
+
+          <a
+            href="https://https://github.com/NickyMasondo/weather-fifth-week"
+            target="blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
